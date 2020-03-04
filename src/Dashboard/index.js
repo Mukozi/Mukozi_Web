a7c1e6d0e50ecdb077e6a26417f450987d1ba59c
Wamala-Emmanuel <kisaakye.emma@gmail.com>
2020-03-04 17:27:10 +0300

    Started work on the dashboard

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,5 +1,4 @@
 $(function() {
   $(".thefooter").load("./../Components/Footer.html");
   $(".theheader").load("./../Components/Header.html");
-  $(".profile_nav").load("./../Components/ProfileNav.html");
 });
